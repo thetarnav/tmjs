f99a4e3a7c3dba3eeb9c81811b35eeeef3e04fcc
Damian Tarnawski <gthetarnav@gmail.com>
2023-12-22 18:59:47 +0100

    Watch build theme

---
+++ build.js
@@ -0,0 +1,26 @@
+import jsonc from 'jsonc-parser'
+import fsp from 'fs/promises'
+import path from 'path'
+import url from 'url'
+import chokidar from 'chokidar'
+
+const dirname = path.dirname(url.fileURLToPath(import.meta.url))
+
+const src_path = path.join(dirname, 'src')
+const theme_jsonc_path = path.join(src_path, 'theme.jsonc')
+const theme_json_path = path.join(src_path, 'theme.json')
+
+async function buildTheme() {
+    const theme_jsonc = await fsp.readFile(theme_jsonc_path, 'utf8')
+    const theme = jsonc.parse(theme_jsonc)
+    const theme_json = JSON.stringify(theme, null, 4)
+
+    await fsp.writeFile(theme_json_path, theme_json, 'utf8')
+}
+
+if (process.argv.includes('--watch')) {
+    chokidar.watch(theme_jsonc_path).on('change', buildTheme)
+    buildTheme()
+} else {
+    buildTheme()
+}
--- src/index.js
+++ src/index.js
@@ -1,31 +1,36 @@
 import * as shikiji from 'shikiji'
 
+const SRC_PATH = 'src/'
+
+/** @type {Promise<string>} */
+const code_promise = fetch(SRC_PATH + 'sample.odin').then(res => res.text())
+
+/** @type {Promise<shikiji.ThemeRegistration>} */
+const theme_promise = fetch(SRC_PATH + 'theme.json').then(res => res.json())
+
 const root = document.getElementById('root')
 if (!root) throw new Error('Root el not found')
 
+const code = await code_promise
+const theme = await theme_promise
+
 const highlighter = await shikiji.getHighlighter({
-    themes: ['nord'],
+    themes: [theme],
     langs: [],
 })
-
-/** @type {Promise<string>} */
-const code_promise = fetch('./sample.odin').then(res => res.text())
 
 /**
  * @returns {Promise<void>}
  */
 const update = async () => {
     /** @type {shikiji.LanguageRegistration} */
-    const lang = await fetch('./odin.tmLanguage.json').then(res => res.json())
-    const code = await code_promise
-
+    const lang = await fetch(SRC_PATH + 'odin.tmLanguage.json').then(res => res.json())
     await highlighter.loadLanguage(lang)
 
     const html = highlighter.codeToHtml(code, {
         lang: lang.name,
-        theme: 'nord',
+        theme: theme.name,
     })
-
     root.innerHTML = html
 }
 
