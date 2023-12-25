49fa57ba07e363b5dcb6000bba5e04f2ff3bdbfe
Damian Tarnawski <gthetarnav@gmail.com>
2023-12-25 15:48:17 +0100

    Renaming odin.tmLanguage.json to tmLanguage.json

--- src/constants.js
+++ src/constants.js
@@ -3,9 +3,9 @@ export const WEB_SOCKET_PORT = 8080
 
 export const THEME_JSONC_FILENAME = 'theme.jsonc'
 export const THEME_JSON_FILENAME = 'theme.json'
-export const LANG_FILENAME = 'odin.tmLanguage.json'
+export const LANG_FILENAME = 'tmLanguage.json'
 export const CODE_FILENAME = 'sample.odin'
 
-export const THEME_JSON_WEBPATH = '/' + 'theme.json'
-export const LANG_WEBPATH = '/' + 'odin.tmLanguage.json'
-export const CODE_WEBPATH = '/' + 'sample.odin'
+export const THEME_JSON_WEBPATH = '/' + THEME_JSON_FILENAME
+export const LANG_WEBPATH = '/' + LANG_FILENAME
+export const CODE_WEBPATH = '/' + CODE_FILENAME
