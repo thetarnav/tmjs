869a7ca98b9e16136cbddd233d8defba71b9f679
Damian Tarnawski <gthetarnav@gmail.com>
2024-10-21 19:23:10 +0200

    Cleanup

--- src/index.js
+++ src/index.js
@@ -75,6 +75,50 @@ function render_tree(scope, src) {
 	)
 }
 
+/**
+@param   {tm.Token[]}      tokens
+@param   {tm.Theme_Item[]} theme_items
+@param   {string}          src
+@returns {void}            */
+function render_tokens(tokens, theme_items, src) {
+
+	/** @type {HTMLElement[]} */
+	let elements = new Array(tokens.length)
+	/** @type {tm.Scope_Theme_Settings_Cache} */
+	let settings_cache = new Map()
+
+	for (let i = 0; i < tokens.length; i += 1) {
+		let token = tokens[i]
+		let settings = tm.match_token_theme(token, theme_items, settings_cache)
+
+		let el = elements[i] = h.span({class: 'token'}, src.slice(token.pos, token.end))
+
+		if (settings.foreground) el.style.color           = settings.foreground
+		if (settings.background) el.style.backgroundColor = settings.background
+		if (settings.fontStyle) {
+			if (settings.fontStyle === 'italic')    el.style.fontStyle      = 'italic'
+			if (settings.fontStyle === 'bold')      el.style.fontWeight     = 'bold'
+			if (settings.fontStyle === 'underline') el.style.textDecoration = 'underline'
+		}
+
+		/*
+		Skip first because it's always the root scope
+		*/
+		let scopes_str = ''
+		for (let i = 1; i < token.scopes.length; i += 1) {
+			let scope = token.scopes[i]
+			if (scope.endsWith('.odin')) {
+				scope = scope.slice(0, -5)
+			}
+			scopes_str += scope + '\n'
+		}
+
+		render_tooltip(el, scopes_str)
+	}
+
+	root.appendChild(h.div({class: 'shiki'}, elements))
+}
+
 const tooltip_el = root.appendChild(h.div({class: 'scope-tooltip'}))
 tooltip_el.style.visibility = 'hidden'
 
@@ -118,190 +162,150 @@ function render_tooltip(el, text) {
 async function main() {
 	loading_indicator.style.display = 'block'
 
-	if (true)
-	{
-		const [code, theme, lang] = await Promise.all([
-			code_promise,
-			theme_promise,
-			lang_promise,
-		])
+	const [code, theme, lang] = await Promise.all([
+		code_promise,
+		theme_promise,
+		lang_promise,
+	])
 
-		if (!tm.validate_json_grammar(lang)) {
-			console.error('Invalid grammar')
-			return
-		}
-
-		let grammar     = tm.json_to_grammar(lang)
-		let theme_items = tm.json_to_theme_items(theme.tokenColors || [], grammar.scope)
-		let tokens      = tm.code_to_tokens(code, grammar)
-
-		setTimeout(() => {
-			let tree = tm.parse_code(code, grammar)
-			render_tree(tree, code)
-		})
-
-		/** @type {HTMLElement[]} */
-		let elements = new Array(tokens.length)
-		/** @type {tm.Scope_Theme_Settings_Cache} */
-		let settings_cache = new Map()
-
-		for (let i = 0; i < tokens.length; i += 1) {
-			let token = tokens[i]
-			let settings = tm.match_token_theme(token, theme_items, settings_cache)
-
-			let el = elements[i] = h.span({class: 'token'}, code.slice(token.pos, token.end))
-
-			if (settings.foreground) el.style.color           = settings.foreground
-			if (settings.background) el.style.backgroundColor = settings.background
-			if (settings.fontStyle) {
-				if (settings.fontStyle === 'italic')    el.style.fontStyle      = 'italic'
-				if (settings.fontStyle === 'bold')      el.style.fontWeight     = 'bold'
-				if (settings.fontStyle === 'underline') el.style.textDecoration = 'underline'
-			}
-
-			/*
-			Skip first because it's always the root scope
-			*/
-			let scopes_str = ''
-			for (let i = 1; i < token.scopes.length; i += 1) {
-				let scope = token.scopes[i]
-				if (scope.endsWith('.odin')) {
-					scope = scope.slice(0, -5)
-				}
-				scopes_str += scope + '\n'
-			}
-
-			render_tooltip(el, scopes_str)
-		}
-
-		root.appendChild(h.div({class: 'shiki'}, elements))
-	}
-	else {
-		const highlighter_promise = shiki.getHighlighterCore({
-			themes:   [theme_promise],
-			langs:    [lang_promise],
-			loadWasm: getShikiWasm,
-		})
-
-		const [code, theme, lang, highlighter] = await Promise.all([
-			code_promise,
-			theme_promise,
-			lang_promise,
-			highlighter_promise,
-		])
-
-		const tokens_lines = highlighter.codeToTokens(code, { // this is slow...
-			lang: lang.name,
-			theme: /** @type {string} */ (theme.name),
-			includeExplanation: true,
-		})
-
-		/** @type {HTMLElement[]} */
-		const elements_lines = new Array(tokens_lines.tokens.length)
-		/** @type {Map<HTMLElement, string>} */
-		const scopes_map = new Map()
-
-		for (let i = 0; i < tokens_lines.tokens.length; i += 1) {
-			const tokens = tokens_lines.tokens[i]
-			/** @type {HTMLElement[]} */
-			const elements = new Array(tokens.length)
-
-			for (let j = 0; j < tokens.length; j += 1) {
-				const token = tokens[j]
-				if (!token.explanation) continue
-
-				const token_el = document.createElement('span')
-				token_el.className = 'token'
-				elements[j] = token_el
-
-				for (const explanation of token.explanation) {
-					const scope_el = document.createElement('span')
-					scope_el.className = 'scope'
-					token_el.append(scope_el)
-
-					scope_el.style.color = token.color || ''
-					scope_el.textContent = explanation.content
-
-					if (token.fontStyle) {
-						if (token.fontStyle & shiki.FontStyle.Italic) {
-							scope_el.style.fontStyle = 'italic'
-						}
-						if (token.fontStyle & shiki.FontStyle.Bold) {
-							scope_el.style.fontWeight = 'bold'
-						}
-						if (token.fontStyle & shiki.FontStyle.Underline) {
-							scope_el.style.textDecoration = 'underline'
-						}
-					}
-
-					/*
-					Skip first because it's always the root scope
-					*/
-					let scope = ''
-					for (let i = 1; i < explanation.scopes.length; i += 1) {
-						let name = explanation.scopes[i].scopeName
-						if (name.endsWith('.odin')) {
-							name = name.slice(0, -5)
-						}
-						scope += name + '\n'
-					}
-
-					scopes_map.set(scope_el, scope)
-				}
-			}
-
-			const line = document.createElement('div')
-			line.className = 'line'
-			elements_lines[i] = line
-			line.append(...elements)
-		}
-
-		const shiki_el = document.createElement('div')
-		shiki_el.className = 'shiki'
-		shiki_el.append(...elements_lines)
-		root.innerHTML = ''
-		root.append(shiki_el)
-
-		/*
-		Show hovered token scope
-		*/
-		const tooltip_el = document.createElement('div')
-		tooltip_el.className = 'scope-tooltip'
-		root.append(tooltip_el)
-
-		/** @type {HTMLElement | null} */
-		let last_scope_el = null
-		shiki_el.addEventListener('mousemove', e => {
-			const target = e.target
-			if (!(target instanceof HTMLElement)) {
-				tooltip_el.style.visibility = 'hidden'
-				last_scope_el = null
-				return
-			}
-
-			if (target !== last_scope_el) {
-				const scope = scopes_map.get(target)
-				if (!scope) {
-					tooltip_el.style.visibility = 'hidden'
-					last_scope_el = null
-					return
-				}
-
-				last_scope_el = target
-				tooltip_el.textContent = scope
-				tooltip_el.style.visibility = 'visible'
-			}
-
-			tooltip_el.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`
-		})
-
-		shiki_el.addEventListener('mouseleave', () => {
-			tooltip_el.style.visibility = 'hidden'
-			last_scope_el = null
-		})
+	if (!tm.validate_json_grammar(lang)) {
+		console.error('Invalid grammar')
+		return
 	}
 
+	let grammar = tm.json_to_grammar(lang)
+	
+	let theme_items = tm.json_to_theme_items(theme.tokenColors || [], grammar.scope)
+	let tokens      = tm.code_to_tokens(code, grammar)
+	render_tokens(tokens, theme_items, code)
 
+	await new Promise(r => setTimeout(r))
+
+	let tree = tm.parse_code(code, grammar)
+	render_tree(tree, code)
+
+	// const highlighter_promise = shiki.getHighlighterCore({
+	// 	themes:   [theme_promise],
+	// 	langs:    [lang_promise],
+	// 	loadWasm: getShikiWasm,
+	// })
+
+	// const [code, theme, lang, highlighter] = await Promise.all([
+	// 	code_promise,
+	// 	theme_promise,
+	// 	lang_promise,
+	// 	highlighter_promise,
+	// ])
+
+	// const tokens_lines = highlighter.codeToTokens(code, { // this is slow...
+	// 	lang: lang.name,
+	// 	theme: /** @type {string} */ (theme.name),
+	// 	includeExplanation: true,
+	// })
+
+	// /** @type {HTMLElement[]} */
+	// const elements_lines = new Array(tokens_lines.tokens.length)
+	// /** @type {Map<HTMLElement, string>} */
+	// const scopes_map = new Map()
+
+	// for (let i = 0; i < tokens_lines.tokens.length; i += 1) {
+	// 	const tokens = tokens_lines.tokens[i]
+	// 	/** @type {HTMLElement[]} */
+	// 	const elements = new Array(tokens.length)
+
+	// 	for (let j = 0; j < tokens.length; j += 1) {
+	// 		const token = tokens[j]
+	// 		if (!token.explanation) continue
+
+	// 		const token_el = document.createElement('span')
+	// 		token_el.className = 'token'
+	// 		elements[j] = token_el
+
+	// 		for (const explanation of token.explanation) {
+	// 			const scope_el = document.createElement('span')
+	// 			scope_el.className = 'scope'
+	// 			token_el.append(scope_el)
+
+	// 			scope_el.style.color = token.color || ''
+	// 			scope_el.textContent = explanation.content
+
+	// 			if (token.fontStyle) {
+	// 				if (token.fontStyle & shiki.FontStyle.Italic) {
+	// 					scope_el.style.fontStyle = 'italic'
+	// 				}
+	// 				if (token.fontStyle & shiki.FontStyle.Bold) {
+	// 					scope_el.style.fontWeight = 'bold'
+	// 				}
+	// 				if (token.fontStyle & shiki.FontStyle.Underline) {
+	// 					scope_el.style.textDecoration = 'underline'
+	// 				}
+	// 			}
+
+	// 			/*
+	// 			Skip first because it's always the root scope
+	// 			*/
+	// 			let scope = ''
+	// 			for (let i = 1; i < explanation.scopes.length; i += 1) {
+	// 				let name = explanation.scopes[i].scopeName
+	// 				if (name.endsWith('.odin')) {
+	// 					name = name.slice(0, -5)
+	// 				}
+	// 				scope += name + '\n'
+	// 			}
+
+	// 			scopes_map.set(scope_el, scope)
+	// 		}
+	// 	}
+
+	// 	const line = document.createElement('div')
+	// 	line.className = 'line'
+	// 	elements_lines[i] = line
+	// 	line.append(...elements)
+	// }
+
+	// const shiki_el = document.createElement('div')
+	// shiki_el.className = 'shiki'
+	// shiki_el.append(...elements_lines)
+	// root.innerHTML = ''
+	// root.append(shiki_el)
+
+	// /*
+	// Show hovered token scope
+	// */
+	// const tooltip_el = document.createElement('div')
+	// tooltip_el.className = 'scope-tooltip'
+	// root.append(tooltip_el)
+
+	// /** @type {HTMLElement | null} */
+	// let last_scope_el = null
+	// shiki_el.addEventListener('mousemove', e => {
+	// 	const target = e.target
+	// 	if (!(target instanceof HTMLElement)) {
+	// 		tooltip_el.style.visibility = 'hidden'
+	// 		last_scope_el = null
+	// 		return
+	// 	}
+
+	// 	if (target !== last_scope_el) {
+	// 		const scope = scopes_map.get(target)
+	// 		if (!scope) {
+	// 			tooltip_el.style.visibility = 'hidden'
+	// 			last_scope_el = null
+	// 			return
+	// 		}
+
+	// 		last_scope_el = target
+	// 		tooltip_el.textContent = scope
+	// 		tooltip_el.style.visibility = 'visible'
+	// 	}
+
+	// 	tooltip_el.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`
+	// })
+
+	// shiki_el.addEventListener('mouseleave', () => {
+	// 	tooltip_el.style.visibility = 'hidden'
+	// 	last_scope_el = null
+	// })
 
 	loading_indicator.style.display = 'none'
 }
